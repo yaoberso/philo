7d2385e62b2edc97d03ccfbf2eb4195941193131
yaoberso <yannoberson@hotmail.com>
2025-02-03 10:11:32 +0100

    Add files via upload

---
+++ philosophers/main.c
@@ -0,0 +1,10 @@
+#include "philosophers.h"
+
+int main(int argc, char **argv)
+{
+	int nb_philo;
+	int temp_mort;
+	int temp_rep;
+	int temp_som;
+	int rep_min;
+}
---
+++ philosophers/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yann <yann@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 13:12:07 by yaoberso          #+#    #+#             */
+/*   Updated: 2025/02/02 15:19:06 by yann             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef philosophers_H
+#define philosophers_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "printf/ft_printf.h"
+
+
+
+#endif
